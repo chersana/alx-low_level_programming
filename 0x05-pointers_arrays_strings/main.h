53edada669a7934f1c58bdb8c20cca6ed8e84d19
Your Name <you@example.com>
2023-03-27 14:14:13 -0700

    completed

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,9 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <studio.h>
-#include <unistd.h>
-
 int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
@@ -15,5 +12,6 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
+
 #endif
 
