88eba5297e487e02b096f6e0cf538f005cddcf17
Your Name <you@example.com>
2023-04-03 13:46:49 -0700

    character in string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _strchr - function that locates a character in a string
+ * @s: string
+ * @c: character
+ * @null: string
+ *
+ * return: char c
+ */
+char *_strchr(char *s, char c)
+{
+	int i, j=0;
+
+	while (s[j])
+	{
+		j++;
+	}
+	for (i=0; i <=j; i++)
+	{
+		if (c == s[i])
+		{
+			s += i;
+			return (s)
+		}	
+	}
+	return ('\0');
+}
