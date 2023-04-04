874e5bc30916b5ab58678047107c507936e76e23
Your Name <you@example.com>
2023-04-03 17:09:42 -0700

    done

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * _strstr - a function that locates a substring
+ * @needle: substring
+ * @haystack: origin string
+ *
+ * Return: beginning of the located substring
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int a = 0, k = 0;
+
+	while (haystack[a])
+	{
+		while (needle[k])
+		{
+			if (haystack[a + k] != needle[k])
+			{
+				break;
+			}
+			k++;
+		}
+
+		if (needle[k] == '\0')
+			{
+				return (haystack + a);
+			}
+			a++;
+	}
+	return ('\0');
+}
