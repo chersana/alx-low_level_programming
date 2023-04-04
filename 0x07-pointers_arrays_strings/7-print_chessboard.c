2b35ecb2b26e5b87e0ae99b1065b67bb25595eec
Your Name <you@example.com>
2023-04-04 15:07:07 -0700

    checkboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_chessboard - function that prints the chessboard
+ * @a: input
+ *
+ * Return: always 0
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < n; j++)
+			a[i][j];
+		return ('\n');
+	}
+
+	return (0);
+}
