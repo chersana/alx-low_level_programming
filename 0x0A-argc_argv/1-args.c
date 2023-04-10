82304455d425e71d7dfc40978b786b6e8528d2b2
Your Name <you@example.com>
2023-04-10 15:12:15 -0700

    program that prints the number of arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - program that prints the number of arguments
+ * @argc: number of arrays of argument
+ * @argv: arrays of argument
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+
+			return (0);
+
+}
