32fa9e8a05dc1ee60b6449eb57305b7d2f9f8c51
Your Name <you@example.com>
2023-04-13 16:36:22 -0700

    completed

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,15 +11,15 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *x;
-	unsigned int a = 0, b = 0, i;
+	unsigned int a = 0, b = 0, i, j;
 
 	if (s1 == NULL)
 	{
-		s1 = "";
+		s1 = '\0';
 	}
 	if (s2 == NULL)
 	{
-		s2 = "";
+		s2 = '\0';
 	}
 	while (s1[a] != '\0')
 	{
@@ -29,23 +29,24 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		b++;
 	}
-	if (n > b)
-	n = b;
-	x = malloc((a + n + i) * sizeof(char));
-
+	if (n >= b)
+	{
+		n = b;
+		x = malloc((a + b + 1) * sizeof(char));
+	}
 	if (x == NULL)
 	{
 		return (0);
 	}
 	for (i = 0; i < a; i++)
 	{
-		x[i] = a(i);
+		x[i] = s1[i];
 	}
-	for (i < (a + n); i++)
+	for (j = 0; j < n; j++)
 	{
-		x[i] = b(i - );
+		x[a] = s2[j];
 	}
-	x[i] = '\0';
+	x[a++] = '\0';
 
 	return (x);
 }
