b1b69947edc02bcaafe103511bb345792d270356
Your Name <you@example.com>
2023-04-20 10:09:18 -0700

    function that returns the sum of all its parameters

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -9,15 +9,15 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	va_list num;
+	va_list a;
 	unsigned int i, sum;
 
-	va_start(num, n);
+	va_start(a, n);
 
 	for (i = 0; i < n; i++)
 	{
-		sum += va_arg(num, int);
+		sum += va_arg(a, int);
 	}
-	va_end(num);
+	va_end(a);
 	return (sum);
 }
---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,15 @@
+#include "variadic_function.h"
+#include <stdio.h>
+#include <stdarg.h>
+/**
+ * print_numbers - function that prints numbers
+ * @separator: string to be printed
+ * @n: number of integers
+ * @...: a variable
+ * 
+ * Return: 0
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list 
+}
