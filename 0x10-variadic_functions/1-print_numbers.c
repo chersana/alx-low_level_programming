bfb2b845c0c6d10663ffc5bc43bb8542d7e0b517
Your Name <you@example.com>
2023-04-20 10:45:33 -0700

    function that prints numbers

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -6,10 +6,25 @@
  * @separator: string to be printed
  * @n: number of integers
  * @...: a variable
- * 
+ *
  * Return: 0
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-	va_list 
+	va_list num;
+	unsigned int i;
+
+	va_start(num, n);
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(num, int));
+
+		if (i != (n - 1) && separator != NULL)
+		{
+			printf("%s", separator);
+		}
+	}
+	printf("\n");
+	va_end(num);
 }
