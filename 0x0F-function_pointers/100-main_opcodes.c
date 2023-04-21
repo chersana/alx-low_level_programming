642d307da02195301c51247bc6e3e9d9b70e2b30
Your Name <you@example.com>
2023-04-21 16:28:55 -0700

    completed

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -9,31 +9,30 @@
  */
 int main(int argc, char *argv[])
 {
-	int b, i;
-	char *array;
+	int bytes, i;
+
+	char *arr;
 
 	if (argc != 2)
 	{
 		printf("Error\n");
 		exit(1);
 	}
-	b = atoi(argv[1]);
-
-	if (b < 0)
+	bytes = atoi(argv[1]);
+	if (bytes < 0)
 	{
-		printf("Erro\n");
+		printf("Error\n");
 		exit(2);
 	}
-	array = (char *)main;
-
-	for (i = 0; i < b; i++)
+	arr = (char *)main;
+	for (i = 0; i < bytes; i++)
 	{
-		if (i == b - 1)
+		if (i == bytes - 1)
 		{
-			printf("%02hhx\n", array[i]);
+			printf("%02hhx\n", arr[i]);
 			break;
 		}
-		printf("%02hhx", array[i]);
+		printf("%02hhx ", arr[i]);
 	}
 	return (0);
 }
