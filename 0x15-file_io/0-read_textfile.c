d2a545d407c094af3cc73a86c73060701d052abd
chersana <chersanauwumugisha1@gmail.com>
2023-05-11 04:19:24 -0700

    0. Tread lightly, she is near

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * read_textfile - function that reads a text file and prints it
+ * @filename: a file
+ * @letters:  number of letters it should read and print
+ *
+ * Return: 0
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *buffer;
+	ssize_t file_descriptor;
+	ssize_t read;
+	ssize_t written;
+
+	file_descriptor = open(filename, O_RDONLY);
+	if (file_descriptor == -1)
+	{
+		return (0);
+	}
+	buffer = malloc(sizeof(char) * letters);
+	read = read(file_descriptor, buffer, letters);
+	written = write(STDOUT_FILENO, buffer, read);
+
+	free(buffer);
+	close(file_descriptor);
+
+	return (written);
+}
